346a0138758064700edf50d3aeedb2e0eaa48b1d
tuffythejeep <>
2024-07-21 15:43:21 -0500

    working index.js

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const Shape = require("./lib/Shape");
 const Triangle = require("./lib/Triangle");
 const Circle = require("./lib/Circle");
 const Square = require("./lib/Square");
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters for your LOGO!!:'
+  },
